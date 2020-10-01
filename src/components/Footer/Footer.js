ac4feae83cdb40836880918615e04004f6a30d9e
Cyrus Jose <cyrus.jose@outlook.com>
2020-10-01 16:23:20 -0700

    Testing footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,5 @@
-import React from 'react'
+import React from "react";
 
 export default function Footer() {
-    return (
-        <div>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia, porro architecto voluptatibus voluptas cupiditate expedita nihil nam iure ratione laudantium deserunt doloremque sint odio aut esse corrupti corporis veniam earum.
-        </div>
-    )
+  return <div>Footer</div>;
 }
