db99ddf7966a956aa478d86936cf226a553651b4
Cyrus Jose <cyrus.jose@outlook.com>
2020-10-01 17:24:09 -0700

    Testing Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,14 @@
 import React from "react";
+import "./Footer.css";
 
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div>
+      <footer className="mt-auto py-3 fixed-bottom">
+        <div className="container">
+          <span className="text-muted">Footer Placement</span>
+        </div>
+      </footer>
+    </div>
+  );
 }
