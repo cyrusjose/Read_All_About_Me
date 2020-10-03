12d13588092af9f544e64aa71fafa07ec36a91ac
Cyrus Jose <cyrus.jose@outlook.com>
2020-10-02 20:53:00 -0700

    Adding styling

--- src/components/Bio/Bio.js
+++ src/components/Bio/Bio.js
@@ -1,9 +1,13 @@
 import React from "react";
 import "./Bio.css";
+import profile from "./images/profile.jpeg"
 export default function Bio() {
   return (
-    <div>
-      
+    <div className="mainBio row">
+      <img src={profile} alt="profile" className="profilePic" />
+      <div className="description">
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint reiciendis quia in, ullam ipsam, laborum dolores reprehenderit dolorum ea molestias facilis, porro sit exercitationem praesentium repudiandae? Porro totam sed iste?</p>
+      </div>
     </div>
   );
 }
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,12 +3,10 @@ import "./Footer.css";
 
 export default function Footer() {
   return (
-    <div>
       <footer className="mt-auto py-3 fixed-bottom">
         <div className="container">
           <span className="text-muted">Footer Placement</span>
         </div>
       </footer>
-    </div>
   );
 }
