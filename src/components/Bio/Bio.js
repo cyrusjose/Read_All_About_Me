0a1fdd6528291577613b3dc5fd9ea3ca53ea503c
Cyrus Jose <cyrus.jose@outlook.com>
2020-10-02 19:21:11 -0700

    made images folder

--- src/components/Bio/Bio.js
+++ src/components/Bio/Bio.js
@@ -1,9 +1,9 @@
-import React from 'react'
-
+import React from "react";
+import "./Bio.css";
 export default function Bio() {
-    return (
-        <div>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum obcaecati, illo voluptatum dignissimos et recusandae eveniet sequi hic soluta velit molestiae iure cum totam magnam fugiat nostrum dolor unde nemo.
-        </div>
-    )
+  return (
+    <div>
+      
+    </div>
+  );
 }
