81ca64b4a3a9f5ef999f7b687139783447327ed8
Cyrus Jose <cyrus.jose@outlook.com>
2020-10-04 15:32:40 -0700

    Fixed margin and padding of contact page

--- src/components/Contact_Info/ContactInfo.js
+++ src/components/Contact_Info/ContactInfo.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import "./Contactinfo.css";
 
 export default function ContactInfo() {
-    return (
-        <div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae eligendi, repudiandae excepturi veniam dolore enim quia ad exercitationem obcaecati aliquid fuga veritatis quod voluptate inventore libero dignissimos hic molestiae aut. 
+  return (
+    <div className="container">
+        <div className="row justify-content-center mt-5 p-5">
+            <div className="card shadow p-3 mb-5 bg-white rounded">
+                <div className="card-body">
+                    <h4 className="card-title mb-4">It would be a pleasure to know you!</h4>
+                    <hr />
+                    <p>
+                        Shoot me an email, I would like to hear more from you.
+                        <br/> Email: <a className="emailLink" href="mailto:cyrus.jose@outlook.com">cyrus.jose@outlook.com</a>.
+                        <br/>
+                        <br/> You can also reach me by phone.
+                        <br/> Phone: <a href="tel: 909-213-1123">(909) 213-1123.</a>
+                    </p>
+                    <a className="emailButton" href="mailto:cyrus.jose@outlook.com"><button className="btn btn-primary" type="Email">Email</button></a>
+                </div>
+            </div>
         </div>
-    )
+    </div>
+  );
 }
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,15 +3,8 @@ import ContactInfo from "../components/Contact_Info/ContactInfo";
 import "./Contact.css";
 export default function Contact() {
   return (
-    <div>
-      <h1>Contact</h1>
+    <div className = "contactInfo">
       <ContactInfo />
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus eos
-        assumenda repellendus doloremque, quis ducimus doloribus quasi
-        blanditiis dolore culpa quae aut nostrum quaerat molestiae delectus
-        aperiam eligendi harum est.
-      </p>
     </div>
   );
 }
