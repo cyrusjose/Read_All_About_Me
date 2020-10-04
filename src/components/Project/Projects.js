50f9e40dba76fafa86b8178c9ee8141c66a955af
Cyrus Jose <cyrus.jose@outlook.com>
2020-10-04 16:55:17 -0700

    styling project

--- src/components/Project/Projects.js
+++ src/components/Project/Projects.js
@@ -1,3 +1,6 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable react/jsx-no-target-blank */
+/* eslint-disable jsx-a11y/img-redundant-alt */
 import React from "react";
 
 import "./Project.css";
@@ -5,7 +8,9 @@ import "./Project.css";
 export default function Projects(props) {
   return (
     <div>
-        
+        <div className="proj1">
+            hello
+        </div>
     </div>
   );
 }
