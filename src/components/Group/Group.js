e7befa0f9a78a34f2bb3ead3eb9bdbcb5dd53b5b
Cyrus Jose <cyrus.jose@outlook.com>
2020-10-04 17:34:23 -0700

    changed class to class name

--- src/components/Group/Group.js
+++ src/components/Group/Group.js
@@ -6,42 +6,42 @@ import "./Group.css";
 export default function Group() {
   return (
     <div>
-      <div class="container mt-5">
-        <div class="row row-cols-1 row-cols-md-2">
-          <div class="col mb-4 p-5">
-            <div class="card portCard ">
+      <div className="container mt-5">
+        <div className="row row-cols-1 row-cols-md-2">
+          <div className="col mb-4 p-5">
+            <div className="card portCard ">
               <a
-                class="topCard"
+                className="topCard"
                 href="https://cyrusjose.github.io/Spontaneous-Weekend/"
                 target="_blank"
               >
                 <img
                   src="https://images.pexels.com/photos/712395/pexels-photo-712395.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-                  class="card-img-top pexelImage img-fluid"
+                  className="card-img-top pexelImage img-fluid"
                   alt="Board Game Image from Pexel"
                 />
-                <div class="card-img-overlay row align-items-end justify-content-center">
-                  <h1 class="card-title">
+                <div className="card-img-overlay row align-items-end justify-content-center">
+                  <h1 className="card-title">
                     <strong>Spontaneous Weekend</strong>
                   </h1>
                 </div>
               </a>
             </div>
           </div>
-          <div class="col mb-4 p-5">
-            <div class="card portCard ">
+          <div className="col mb-4 p-5">
+            <div className="card portCard ">
               <a
-                class="topCard"
+                className="topCard"
                 href="https://github.com/cyrusjose/Spontaneous-Weekend"
                 target="_blank"
               >
                 <img
                   src="https://images.pexels.com/photos/712395/pexels-photo-712395.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-                  class="card-img-top pexelImage img-fluid"
+                  className="card-img-top pexelImage img-fluid"
                   alt="Board Game Image from Pexel"
                 />
-                <div class="card-img-overlay row align-items-end justify-content-center">
-                  <h1 class="card-title">
+                <div className="card-img-overlay row align-items-end justify-content-center">
+                  <h1 className="card-title">
                     <strong>Spontaneous Weekend (github)</strong>
                   </h1>
                 </div>
@@ -49,41 +49,41 @@ export default function Group() {
             </div>
           </div>
         </div>
-        <div class="row row-cols-1 row-cols-md-2">
-          <div class="col mb-4 p-5">
-            <div class="card portCard ">
+        <div className="row row-cols-1 row-cols-md-2">
+          <div className="col mb-4 p-5">
+            <div className="card portCard ">
               <a
-                class="topCard"
+                className="topCard"
                 href="https://secure-savannah-12538.herokuapp.com/signup.html"
                 target="_blank"
               >
                 <img
                   src="https://images.pexels.com/photos/712395/pexels-photo-712395.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-                  class="card-img-top pexelImage img-fluid"
+                  className="card-img-top pexelImage img-fluid"
                   alt="Board Game Image from Pexel"
                 />
-                <div class="card-img-overlay row align-items-end justify-content-center">
-                  <h1 class="card-title">
+                <div className="card-img-overlay row align-items-end justify-content-center">
+                  <h1 className="card-title">
                     <strong>Spontaneous Weekend 2.0</strong>
                   </h1>
                 </div>
               </a>
             </div>
           </div>
-          <div class="col mb-4 p-5">
-            <div class="card portCard ">
+          <div className="col mb-4 p-5">
+            <div className="card portCard ">
               <a
-                class="topCard"
+                className="topCard"
                 href="https://github.com/melindawinter/Spontaneous-Weekend"
                 target="_blank"
               >
                 <img
                   src="https://images.pexels.com/photos/712395/pexels-photo-712395.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-                  class="card-img-top pexelImage img-fluid"
+                  className="card-img-top pexelImage img-fluid"
                   alt="Board Game Image from Pexel"
                 />
-                <div class="card-img-overlay row align-items-end justify-content-center">
-                  <h1 class="card-title">
+                <div className="card-img-overlay row align-items-end justify-content-center">
+                  <h1 className="card-title">
                     <strong>Spontaneous Weekend 2.0 (github)</strong>
                   </h1>
                 </div>
--- src/components/Project/Projects.js
+++ src/components/Project/Projects.js
@@ -8,18 +8,18 @@ import { Link } from "react-router-dom";
 export default function Projects(props) {
   return (
     <div>
-      <div class="container mt-5">
-        <div class="row row-cols-1 row-cols-md-2">
-          <div class="col mb-4 p-5">
-            <div class="card portCard ">
+      <div className="container mt-5">
+        <div className="row row-cols-1 row-cols-md-2">
+          <div className="col mb-4 p-5">
+            <div className="card portCard ">
                 <Link className = "topCard" to = "/groupprojects">
                 <img
                   src="https://images.pexels.com/photos/3626622/pexels-photo-3626622.jpeg?cs=srgb&dl=group-of-people-in-a-meeting-3626622.jpg&fm=jpg"
-                  class="card-img-top pexelImage img-fluid"
+                  className="card-img-top pexelImage img-fluid"
                   alt="Group Image From Pexel"
                 />
-                <div class="card-img-overlay row align-items-end justify-content-center">
-                  <h3 class="card-title">
+                <div className="card-img-overlay row align-items-end justify-content-center">
+                  <h3 className="card-title">
                     <strong>Group Projects</strong>
                   </h3>
                 </div>
@@ -27,120 +27,120 @@ export default function Projects(props) {
                 
             </div>
           </div>
-          <div class="col mb-4 p-5">
-            <div class="card portCard ">
+          <div className="col mb-4 p-5">
+            <div className="card portCard ">
               <a
-                class="topCard"
+                className="topCard"
                 href="https://damp-falls-15705.herokuapp.com/"
                 target="_blank"
               >
                 <img
                   src="https://images.pexels.com/photos/1199957/pexels-photo-1199957.jpeg?cs=srgb&dl=pexels-valeria-boltneva-1199957.jpg&fm=jpg"
-                  class="card-img-top pexelImage img-fluid"
+                  className="card-img-top pexelImage img-fluid"
                   alt="Computer Image From Pexel"
                 />
-                <div class="card-img-overlay row align-items-end justify-content-center">
-                  <h3 class="card-title">
+                <div className="card-img-overlay row align-items-end justify-content-center">
+                  <h3 className="card-title">
                     <strong>Hamburgers!</strong>
                   </h3>
                 </div>
               </a>
             </div>
           </div>
-          <div class="col mb-4 p-5">
-            <div class="card portCard ">
+          <div className="col mb-4 p-5">
+            <div className="card portCard ">
               <a
-                class="topCard"
+                className="topCard"
                 href="https://cyrusjose.github.io/Code-Quiz/"
                 target="_blank"
               >
                 <img
                   src="https://images.pexels.com/photos/2740954/pexels-photo-2740954.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
-                  class="card-img-top pexelImage img-fluid"
+                  className="card-img-top pexelImage img-fluid"
                   alt="Computer Image From Pexel"
                 />
-                <div class="card-img-overlay row align-items-end justify-content-center">
-                  <h3 class="card-title">
+                <div className="card-img-overlay row align-items-end justify-content-center">
+                  <h3 className="card-title">
                     <strong>Code Quiz</strong>
                   </h3>
                 </div>
               </a>
             </div>
           </div>
-          <div class="col mb-4 p-5">
-            <div class="card portCard ">
+          <div className="col mb-4 p-5">
+            <div className="card portCard ">
               <a
-                class="topCard"
+                className="topCard"
                 href="https://cyrusjose.github.io/WeatherDashboard/"
                 target="_blank"
               >
                 <img
                   src="https://images.pexels.com/photos/2310713/pexels-photo-2310713.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
-                  class="card-img-top pexelImage img-fluid"
+                  className="card-img-top pexelImage img-fluid"
                   alt="Clouds Image From Pexel"
                 />
-                <div class="card-img-overlay row align-items-end justify-content-center">
-                  <h3 class="card-title">
+                <div className="card-img-overlay row align-items-end justify-content-center">
+                  <h3 className="card-title">
                     <strong>Weather App</strong>
                   </h3>
                 </div>
               </a>
             </div>
           </div>
-          <div class="col mb-4 p-5">
-            <div class="card portCard ">
+          <div className="col mb-4 p-5">
+            <div className="card portCard ">
               <a
-                class="topCard"
+                className="topCard"
                 href="../assets/Simple-Apps/RPS/rps.html"
                 target="_blank"
               >
                 <img
                   src="https://images.pexels.com/photos/133372/pexels-photo-133372.jpeg?cs=srgb&dl=balance-balancing-boulder-close-up-133372.jpg&fm=jpg"
-                  class="card-img-top pexelImage img-fluid"
+                  className="card-img-top pexelImage img-fluid"
                   alt="Board Game Image from Pexel"
                 />
-                <div class="card-img-overlay row align-items-end justify-content-center">
-                  <h3 class="card-title">
+                <div className="card-img-overlay row align-items-end justify-content-center">
+                  <h3 className="card-title">
                     <strong>RPS Game</strong>
                   </h3>
                 </div>
               </a>
             </div>
           </div>
-          <div class="col mb-4 p-5">
-            <div class="card portCard ">
+          <div className="col mb-4 p-5">
+            <div className="card portCard ">
               <a
-                class="topCard"
+                className="topCard"
                 href="../assets/Simple-Apps/Questions/question.html"
                 target="_blank"
               >
                 <img
                   src="https://images.pexels.com/photos/3683107/pexels-photo-3683107.jpeg?cs=srgb&dl=question-mark-on-yellow-background-3683107.jpg&fm=jpg"
-                  class="card-img-top pexelImage img-fluid"
+                  className="card-img-top pexelImage img-fluid"
                   alt="Question Mark"
                 />
-                <div class="card-img-overlay row align-items-end justify-content-center">
-                  <h3 class="card-title">
+                <div className="card-img-overlay row align-items-end justify-content-center">
+                  <h3 className="card-title">
                     <strong>Questions Game</strong>
                   </h3>
                 </div>
               </a>
             </div>
           </div>
-          <div class="col mb-4 p-5">
-            <div class="card portCard ">
+          <div className="col mb-4 p-5">
+            <div className="card portCard ">
               <a
-                class="topCard"
+                className="topCard"
                 href="../assets/Simple-Apps/Car/carGame.html"
                 target="_blank"
               >
                 <img
                   src="https://images.pexels.com/photos/799443/pexels-photo-799443.jpeg?cs=srgb&dl=forced-perspective-photography-of-cars-running-on-road-below-799443.jpg&fm=jpg"
-                  class="card-img-top pexelImage img-fluid"
+                  className="card-img-top pexelImage img-fluid"
                   alt="Cars and Phone Image"
                 />
-                <div class="card-img-overlay row align-items-end justify-content-center">
-                  <h3 class="card-title">
+                <div className="card-img-overlay row align-items-end justify-content-center">
+                  <h3 className="card-title">
                     <strong>Around The World</strong>
                   </h3>
                 </div>
